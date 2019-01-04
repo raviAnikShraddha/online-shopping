0ae77247711e7bcf3d14255fb7ad710838a36bbd
raviAnikShraddha <ravipratapkashyap@gmail.com>
2019-01-04 19:09:13 +0530

    fixing the bug for zero product out of stock !

--- onlineshoppingfrontend/src/main/webapp/assets/js/myapp.js
+++ onlineshoppingfrontend/src/main/webapp/assets/js/myapp.js
@@ -63,7 +63,13 @@ $(function() {
 								}
 							},
 							{
-								data : 'quantity'
+								data : 'quantity',
+								mRender : function(data, type, row) {
+									if (data < 1) {
+										return '<span style="color:red">Out of stock!</span>'
+									}
+									return data;
+								}
 							},
 							{
 								data : 'id',
@@ -76,11 +82,20 @@ $(function() {
 											+ '/show/'
 											+ data
 											+ '/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';
-									str += '<a href="'
-											+ window.contextRoot
-											+ '/cart/add/'
-											+ data
-											+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+
+									if (row.quantity < 1) {
+
+										str += '<a href="javascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+
+									} else {
+
+										str += '<a href="'
+												+ window.contextRoot
+												+ '/cart/add/'
+												+ data
+												+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+
+									}
 
 									return str;
 								}
